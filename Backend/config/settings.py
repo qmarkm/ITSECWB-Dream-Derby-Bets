4fb0c329ccbca6628f99d5bb42dcf4e7f2c85ad9
qmarkm <106965891+qmarkm@users.noreply.github.com>
2026-04-09 23:08:02 +0800

    Trying to Fix production API URL for CORS 2

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')  # No default — server will refuse to start 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
